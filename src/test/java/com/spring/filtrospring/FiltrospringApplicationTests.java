6ad22f31c957d6bb6ba6f34ea2ca4f8d7c80ed9e
SebastianVergaraVelasquez <sv.velasquez36@gmail.com>
2024-08-13 06:47:18 -0500

    feat: :tada: Init

---
+++ src/test/java/com/spring/filtrospring/FiltrospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.filtrospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiltrospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
